0ed411c136cd3a3a239740ede3b80ac022018211
Enterprise09 <goldenstaytes@gmail.com>
2022-01-28 00:33:11 +0900

    [+] commnet

--- quiz_2588.py
+++ quiz_2588.py
@@ -1,6 +1,9 @@
+# https://www.acmicpc.net/problem/2588
+
 a = int(input())
 b = int(input())
 
+# 각 자리의 수를 구함
 s = int(b % 10)
 m = int((b % 100) / 10)
 l = int(b / 100)
@@ -8,7 +11,7 @@ l = int(b / 100)
 n3 = a * s
 n4 = a * m
 n5 = a * l
-total = n3 + n4 * 10 + n5 * 100
+total = n3 + n4 * 10 + n5 * 100  # 총합 계산
 
 print(n3)
 print(n4)
