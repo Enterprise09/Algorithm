8c38acb70fb41d974910d4a950992080eb10a897
Enterprise09 <goldenstaytes@gmail.com>
2022-01-28 00:18:23 +0900

    [solved] 2588

--- quiz_2588.py
+++ quiz_2588.py
@@ -1,1 +1,16 @@
-print('hello')
+a = int(input())
+b = int(input())
+
+s = int(b % 10)
+m = int((b % 100) / 10)
+l = int(b / 100)
+
+n3 = a * s
+n4 = a * m
+n5 = a * l
+total = n3 + n4 * 10 + n5 * 100
+
+print(n3)
+print(n4)
+print(n5)
+print(total)
