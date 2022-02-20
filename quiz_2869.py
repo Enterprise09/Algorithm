6b222992b023284f61d1fc218bec83c3473a1cb9
Enterprise09 <goldenstaytes@naver.com>
2022-02-20 23:40:02 +0900

    [solved] 달팽이는 올라가고 싶다 - 2869

--- quiz_2869.py
+++ quiz_2869.py
@@ -1,11 +1,11 @@
 # https://www.acmicpc.net/problem/2869
 
+from math import ceil
+
 a, b, v = map(int, input().split())
-sum = 0
-ctr = 0
-while sum < v:
-    sum += a - b
-    if(sum == v):
-        break
-    ctr += 1
-print(ctr)
+# a만큼 올라가고 b만큼 떨어지는 것을 n일 만큼 반복하고 
+# 마지막 날에는 a만큼 올라가고 내려오지 않음
+# (a - b) * n + a = v
+
+result = ceil((v - a) / (a - b)) + 1
+print(result)
